a62504c0acf989c570f2a3a321c5626b35ab99a2
KhalFOil <khaliljaouhari456@gmail.com>
2023-06-22 15:55:55 -0700

    okey

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,10 +3,28 @@
 /**
  * main - Entry point of the program
  *
- * Return: 0
+ * Description: This function serves as the entry point of the program.
+ *              It prints a message indicating the avoidance of an infinite loop.
+ *
+ * Return: Always 0 (indicating success)
  */
 int main(void)
 {
-printf("Hello, world!\n");
-return (0);
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*
+	 * while (i < 10)
+	 * {
+	 *	putchar(i);
+	 *	i++;
+	 * }
+	 */
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
