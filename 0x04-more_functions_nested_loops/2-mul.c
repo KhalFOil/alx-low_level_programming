6cba0bfc0f1fff9017a5d1ff6515a00f2052e2b2
KhalFOil <khaliljaouhari456@gmail.com>
2023-06-23 15:32:03 -0700

    sdsd

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - Entry point of the program
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
     printf("%d\n", mul(98, 1024));
     printf("%d\n", mul(-402, 4096));
+
     return (0);
 }
